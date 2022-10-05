ccde92d506a8c9ace0d777acf551d9b558f1b4bb
Юля <potemkin.vg@yandex.ru>
2022-10-05 23:38:45 +0300

    Задание 1 выполнено.

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        //Задание 1
+        System.out.println("Задание 1");
+       int clientOS = 1;
+       if (clientOS == 0) {
+           System.out.println("Установите версию приложения для iOS по ссылке");
+       } else {
+           System.out.println("Установите версию приложения для Android по ссылке");
+       }
+    }{
+    // Задание 2
+        System.out.println("Задание 2");
+
+
+}
+}
