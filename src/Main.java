7c8f63ebcdd6cea95de9c57e82db6697b3039639
Юля <potemkin.vg@yandex.ru>
2022-10-06 01:05:41 +0300

    Задание 4 выполнено.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Main {
     public static void main(String[] args) {
         //Задание 1
@@ -19,10 +21,30 @@ public class Main {
             } else if (clientDeviceYear < 2015) {
                 System.out.println("Установите облегченную версию приложения для iOS по ссылке");
             }
-        if (clientOS1 ==1)
+        if (clientOS1 == 1)
             if (clientDeviceYear >= 2015) {
-            System.out.println("Установите версию приложения для Android по ссылке");
+                System.out.println("Установите версию приложения для Android по ссылке");
             } else if (clientDeviceYear < 2015) {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
+        {
+            // Задание 3
+            System.out.println("Задание 3");
+            // я бы сделала следующим образом, если каждый четвертый год високосный, то число, которое цело делится на 4, за исключением 100-ого. Если наш год делится цело на 4 без остатка, но при этом может быть равен 400, 800, 1200 и т.д., то он високосный
+            // Но таких переменных я не знаю.
         }
-}}
+
+        //Задание 4
+        System.out.println("Задание 4");
+        int deliveryDistance = 95;
+        if (deliveryDistance < 20) {
+            System.out.println("Потребуется 1 день");
+        }        if (deliveryDistance >= 20 && deliveryDistance < 60) {
+            System.out.println("Потребуется 2 дня");
+        }        if (deliveryDistance >= 60 && deliveryDistance < 100){
+            System.out.println("Потребуется 3 дня");
+
+        }
+    }
+}
+
