6f06af2a80902a8b27b6c5e38dbf40fc1f686180
Юля <potemkin.vg@yandex.ru>
2022-10-06 01:21:04 +0300

    Задание 5 выполнено.

--- src/Main.java
+++ src/Main.java
@@ -45,6 +45,36 @@ public class Main {
             System.out.println("Потребуется 3 дня");
 
         }
+        //Задание 5
+        System.out.println("Задание 5");
+        int monthNumber = 13;
+        switch (monthNumber) {
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+
+            default:
+                if (monthNumber > 12) {
+                System.out.println("Такого месяца не существует");
+            }
+        }
     }
 }
 
