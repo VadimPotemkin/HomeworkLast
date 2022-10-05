392519192f133c36b48a8e3ad703ffea42045e61
Юля <potemkin.vg@yandex.ru>
2022-10-06 00:32:28 +0300

    Задание 2 выполнено.

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,27 @@ public class Main {
     public static void main(String[] args) {
         //Задание 1
         System.out.println("Задание 1");
-       int clientOS = 1;
-       if (clientOS == 0) {
-           System.out.println("Установите версию приложения для iOS по ссылке");
-       } else {
-           System.out.println("Установите версию приложения для Android по ссылке");
-       }
-    }{
-    // Задание 2
+        int clientOS = 1;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
+        // Задание 2
         System.out.println("Задание 2");
-
-
-}
-}
+        int clientOS1 = 1;
+        int clientDeviceYear = 2015;
+        if (clientOS1 == 0)
+            if (clientDeviceYear >= 2015) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            } else if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            }
+        if (clientOS1 ==1)
+            if (clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+            } else if (clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+}}
